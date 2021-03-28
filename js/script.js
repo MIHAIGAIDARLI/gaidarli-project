1bdc71e1313c55cdca3f2f69980b9af2064cc9d9
Misa <gaidarli04@mail.ru>
2021-03-28 12:27:07 +0300

    all of films

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оценмте его?', '');
+
+          if(a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+            personalMovieDB.movies[a] = b;
+            console.log('done')
+          } else {
+              console.log('error'),
+              i--
+          }
+}
