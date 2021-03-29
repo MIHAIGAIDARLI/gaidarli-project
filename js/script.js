642b41a9b0bad94993ea2aab77d30f5e8b06d571
Misa <gaidarli04@mail.ru>
2021-03-29 13:12:05 +0300

    while

--- js/script.js
+++ js/script.js
@@ -7,18 +7,25 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оценмте его?', '');
+    let i = 0
+    while ( i < 2 ) {
+        a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оценмте его?', '');
+        i++
+    
 
           if(a != null && b != null && a != '' && b != '' && a.length < 50 ) {
             personalMovieDB.movies[a] = b;
-            console.log('done')
+            console.log('done');
           } else {
-              console.log('error'),
-              i--
+              console.log('error');
+              i--;
           }
-}
+
+        }
+    
+        
+
 
 if(personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмо')
